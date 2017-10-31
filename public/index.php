8c919f0ef5c08995e1d03ceef8aed50fb20e1f86
tauzin <476531330@qq.com>
2017-10-31 15:18:55 +0800

    增加基于swoole的http服务器

--- public/index.php
+++ public/index.php
@@ -1,2 +1,17 @@
 <?php
-echo 'Hello world';
+/**
+ * HTTP服务器
+ * swoole_http_server 8088
+ */
+$http = new swoole_http_server("0.0.0.0",8088);
+
+$http->on("start", function ($server) {
+    echo "Swoole http server is started at http://127.0.0.1:8088\n";
+});
+
+$http->on("request", function ($request, $response) {
+    $response->header("Content-Type", "text/plain");
+    $response->end("Hello World\n".json_encode(['hello'=>'nihao'.time()]));
+});
+
+$http->start();
