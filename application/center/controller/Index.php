4a471c9c8aa22b002e294a26c4ee335e181a0da9
tauzin <476531330@qq.com>
2017-11-18 16:40:15 +0800

    登录页面细节修改

--- application/center/controller/Index.php
+++ application/center/controller/Index.php
@@ -9,19 +9,6 @@ class Index extends Controller
      */
     public function index()
     {
-//        $a = Db::name('service')->alias('a')->join('consumer b','b.service = a.name')->where([
-//            ['a.name','in','UserService,LoginService'],
-//            ['b.status','=',1]
-//        ])->field('b.ip,b.notify_port')->distinct(true)->select();
-//        halt($a);
-        $service = Db::name('service')->where([
-            ['name','in','UserService,LoginService'],
-            ['status','=',1]
-        ])
-        ->field('id,start_time,stop_time,status',true)
-            ->order('name')
-        ->select();
-        halt($service);
         return $this->fetch();
     }
 
